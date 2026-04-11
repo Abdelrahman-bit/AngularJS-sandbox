fd25854026b9cd8562d0f35eb1708d42ea61c1c2
Abdelrahman-bit <abdolela670@gmail.com>
2026-04-11 13:08:54 +0200

    interducing "ng-include" and ("ng-show", "ng-hide") and the right way of using controllers.

--- 3-modules/app/app.js
+++ 3-modules/app/app.js
@@ -21,12 +21,18 @@ app.run(function () {
 app.controller("firstController", [ '$scope', ($scope) => {
 	// a controller is a function that fire when the controller is loaded, so we can use it to set up some stuff when the controller is ready
 	// thing like setting up some variables, or even setting up some third party libraries
-	directives = [
+	const directives = [
 		{ name: "ng-app", description: "app module tells Angular to bootstrap the application" },
 		{ name: "ng-Init", description: "initializes the Angular application, and evaluates the expression" },
 		{ name: "ng-repeat", description: "repeats HTML elements for each item in a collection, syntax: 'name in names' and then use the variable name to access the item" },
 		{ name: "ng-model", description: "binds the value of HTML controls to application data. Like text inputs" },
+		{ name: "ng-include", description: "includes an external HTML file into the current HTML file" },
+		{ name: "ng-show", description: "shows or hides an HTML element based on a condition" },
+		{ name: "ng-hide", description: "shows or hides an HTML element based on a condition" },
+		{ name: "ng-click", description: "binds a click event to an HTML element" },
 	];
+
+	
 	// $scope.directives = directives;
 	// $scope.name = "John Doe";
 	Object.assign($scope, {
@@ -34,3 +40,25 @@ app.controller("firstController", [ '$scope', ($scope) => {
 		name: "John Doe",
 	});
 }]);
+
+app.controller("secondController", [ '$scope', ($scope)=> {
+	const grocery = [
+		{ name: "Milk", price: 1.5, quantity: 2, inStock: true },
+		{ name: "Bread", price: 2, quantity: 1, inStock: true },
+		{ name: "Eggs", price: 0.5, quantity: 0, inStock: false },
+		{ name: "Cheese", price: 3, quantity: 1, inStock: true },
+		{ name: "Butter", price: 2, quantity: 0, inStock: false },
+	]; 
+
+	function removeItem(item){
+		const index = grocery.indexOf(item);
+		if(index > -1){
+			grocery.splice(index, 1);
+		}
+	}
+
+	Object.assign($scope, {
+		grocery,
+		removeItem,
+	});
+}])
