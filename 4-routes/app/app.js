a79c06f54fffff30fde8ed3fb7365f71d3582669
Abdelrahman-bit <abdolela670@gmail.com>
2026-04-11 17:54:53 +0200

    sitting up angular router. and configure routes

---
+++ 4-routes/app/app.js
@@ -0,0 +1,97 @@
+const app = angular.module("firstApp", ['ngRoute']);
+// the array is for minification purposes, to prevent the names of the dependencies from being changed
+// You can think of a module as a container for all our components: like (controllers, configs, methods)
+
+app.config(['$routeProvider', function($routeProvider) {
+	// a config is a function that fire before the app is fully loaded, so we can use it to set up some stuff before the app is ready
+	// thing like routing, or setting up some global variables, or even setting up some third party libraries
+	$routeProvider
+		.when("/directives", {
+			templateUrl: "views/directives.html",
+			controller: "firstController",
+		})
+		.when("/grocery", {
+			templateUrl: "views/grocery.html",
+			controller: "secondController",
+		})
+		.when('/home', {
+			templateUrl: "views/home.html",
+		})
+		.otherwise({
+			redirectTo: "/",
+		});
+	console.log("config fired");
+}]);
+
+app.run(function () {
+	var time = Date.now();
+	// a run is a function that fire after the app is fully loaded, so we can use it to set up some stuff after the app is ready
+	// thing like setting up some global variables, or even setting up some third party libraries
+	console.log("run fired");
+	console.log(Date.now() - time);
+});
+
+app.controller("firstController", [ '$scope', function($scope){
+	// a controller is a function that fire when the controller is loaded, so we can use it to set up some stuff when the controller is ready
+	// thing like setting up some variables, or even setting up some third party libraries
+	const directives = [
+		{ name: "ng-app", description: "app module tells Angular to bootstrap the application" },
+		{ name: "ng-Init", description: "initializes the Angular application, and evaluates the expression" },
+		{ name: "ng-repeat", description: "repeats HTML elements for each item in a collection, syntax: 'name in names' and then use the variable name to access the item" },
+		{ name: "ng-model", description: "binds the value of HTML controls to application data. Like text inputs" },
+		{ name: "ng-include", description: "includes an external HTML file into the current HTML file" },
+		{ name: "ng-show", description: "shows or hides an HTML element based on a condition" },
+		{ name: "ng-hide", description: "shows or hides an HTML element based on a condition" },
+		{ name: "ng-click", description: "binds a click event to an HTML element" },
+		{ name: "ng-submit", description: "binds a submit event to an HTML form" },
+		{ name: "ng-src", description: "binds the src attribute of an HTML element to a variable, preventing hydration errors on the browser" },
+		{ name: "ng-href", description: "binds the href attribute of an HTML element to a variable, preventing hydration errors on the browser" },
+		{ name: "ng-route", description: "used to define routes in the application, in the module dependancy array" },
+	];
+
+	console.log(angular.toJson(directives))
+	console.log(JSON.stringify(directives))
+	
+	// $scope.directives = directives;
+	// $scope.name = "John Doe";
+	Object.assign($scope, {
+		directives,
+		name: "John Doe",
+	});
+}]);
+
+app.controller("secondController", [ '$scope', function($scope){
+	const grocery = [
+		{ name: "Milk", price: 1.5, quantity: 2, inStock: true },
+		{ name: "Bread", price: 2, quantity: 1, inStock: true },
+		{ name: "Eggs", price: 0.5, quantity: 0, inStock: false },
+		{ name: "Cheese", price: 3, quantity: 1, inStock: true },
+		{ name: "Butter", price: 2, quantity: 0, inStock: false },
+	]; 
+
+	function removeItem(item){
+		const index = grocery.indexOf(item);
+		if(index > -1){
+			grocery.splice(index, 1);
+		}
+	}
+
+	const addItem = ()=>{
+		if(!$scope.newItem.name || !$scope.newItem.price || !$scope.newItem.quantity){
+			return alert("Please fill all the fields");
+		}
+		grocery.push({
+			name: $scope.newItem.name,
+			price: parseFloat($scope.newItem.price),
+			quantity: $scope.newItem.quantity,
+			inStock: $scope.newItem.quantity > 0,
+		});
+		$scope.newItem = {};
+	}
+
+	Object.assign($scope, {
+		grocery,
+		removeItem,
+		addItem,
+	});
+}])
